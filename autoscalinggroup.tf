6773ebe850787cf76bc0b45cb0fb97266d0b8951
imvijaysingh <59255347+imvijaysingh@users.noreply.github.com>
2020-10-03 02:04:34 +0530

    Update autoscalinggroup.tf

--- autoscalinggroup.tf
+++ autoscalinggroup.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "wordpress" {
   availability_zones = ["ap-south-1"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
+  desired_capacity   = 2
+  max_size           = 3
+  min_size           = 2
 }
